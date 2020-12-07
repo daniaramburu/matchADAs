200980f6ad061decf585803c78e9cfde17ade4ef
IvetteMery <ivette.mery@gmail.com>
2020-12-07 16:46:59 -0300

    agregue funcion son adyacentes

--- main.js
+++ main.js
@@ -12,83 +12,77 @@ const contenedorBotonDificil = document.getElementById(
 const gatitosSeleccionados = document.querySelectorAll(".seleccionado");
 let gatitoGuardadoEnClickAnterior = null;
 
-
 botonFacil.onclick = () => {
-     comenzarJuegoSinMatchesFacil();
-     ocultarSeleccionDificultad()
-   
-    // reiniciarJuego.classList.add("facil");
-  };
-  
-  botonMedio.onclick = () => {
-     comenzarJuegoSinMatchesMedio();
-     ocultarSeleccionDificultad()
-  
-    // ocultarBotones();
-    // reiniciarJuego.classList.add("medio");
-  };
-  
-  botonDificil.onclick = () => {
-     comenzarJuegoSinMatchesDificil();
-     ocultarSeleccionDificultad()
-    // reiniciarJuego.classList.add("dificil");
-    // clickeable();
-    // console.log(clickeable());
-  };
-  
+  comenzarJuegoSinMatchesFacil();
+  ocultarSeleccionDificultad();
 
-  const comenzarJuegoSinMatchesFacil = () => {
-    do {
-      console.log(generarGrilla(9, 9));
-      generarGrilla(9, 9);
-      agregarGrillaAHTML(9, 9);
-    } while (buscarBloqueInicial());
-  };
-  
-  const comenzarJuegoSinMatchesMedio = () => {
-    do {
-      // vaciarGrilla();
-      generarGrilla(8, 8);
-      agregarGrillaAHTML(8, 8);
-    } while (buscarBloqueInicial());
-  };
-  
-  const comenzarJuegoSinMatchesDificil = () => {
-    do {
-      //vaciarGrilla();
-      console.log(generarGrilla(7, 7))
-      generarGrilla(7, 7);
-      agregarGrillaAHTML(7, 7);
-    } while (buscarBloqueInicial());
-  };
+  // reiniciarJuego.classList.add("facil");
+};
 
+botonMedio.onclick = () => {
+  comenzarJuegoSinMatchesMedio();
+  ocultarSeleccionDificultad();
 
+  // ocultarBotones();
+  // reiniciarJuego.classList.add("medio");
+};
 
-  const buscarBloqueInicial = () => {
-    for (let i = 0; i < grilla.length; i++) {
-      for (let j = 0; j < grilla[i].length; j++) {
-        if (
-          grilla[i][j] === grilla[i][j + 1] &&
-          grilla[i][j + 1] === grilla[i][j + 2]
-        ) {
-          return true;
-        }
-        if (
-          grilla[i + 1] &&
-          grilla[i + 2] &&
-          grilla[i][j] === grilla[i + 1][j] &&
-          grilla[i][j] === grilla[i + 2][j]
-        ) {
-          return true;
-        }
+botonDificil.onclick = () => {
+  comenzarJuegoSinMatchesDificil();
+  ocultarSeleccionDificultad();
+  // reiniciarJuego.classList.add("dificil");
+  // clickeable();
+  // console.log(clickeable());
+};
+
+const comenzarJuegoSinMatchesFacil = () => {
+  do {
+    console.log(generarGrilla(9, 9));
+    generarGrilla(9, 9);
+    agregarGrillaAHTML(9, 9);
+  } while (buscarBloqueInicial());
+};
+
+const comenzarJuegoSinMatchesMedio = () => {
+  do {
+    // vaciarGrilla();
+    generarGrilla(8, 8);
+    agregarGrillaAHTML(8, 8);
+  } while (buscarBloqueInicial());
+};
+
+const comenzarJuegoSinMatchesDificil = () => {
+  do {
+    //vaciarGrilla();
+    console.log(generarGrilla(7, 7));
+    generarGrilla(7, 7);
+    agregarGrillaAHTML(7, 7);
+  } while (buscarBloqueInicial());
+};
+
+const buscarBloqueInicial = () => {
+  for (let i = 0; i < grilla.length; i++) {
+    for (let j = 0; j < grilla[i].length; j++) {
+      if (
+        grilla[i][j] === grilla[i][j + 1] &&
+        grilla[i][j + 1] === grilla[i][j + 2]
+      ) {
+        return true;
+      }
+      if (
+        grilla[i + 1] &&
+        grilla[i + 2] &&
+        grilla[i][j] === grilla[i + 1][j] &&
+        grilla[i][j] === grilla[i + 2][j]
+      ) {
+        return true;
       }
     }
-    return false;
-  };
+  }
+  return false;
+};
 
-
-
-const items = ["🐔", "🐣", "🐤", "🐥",'🦉','🦢'];
+const items = ["🐔", "🐣", "🐤", "🐥", "🦉", "🦢"];
 
 let grilla = [];
 
@@ -143,6 +137,25 @@ const agregarGrillaAHTML = (ancho) => {
   console.log("aca esta la lista de mierda", listaDeEmojis);
 };
 
+//funcion que retorna si dos cuadrados son adyacentes
+const sonAdyacentes = (cuadrado1, cuadrado2) => {
+  const datax1 = Number(cuadrado1.dataset.x);
+  const datax2 = Number(cuadrado2.dataset.x);
+  const datay1 = Number(cuadrado1.dataset.y);
+  const datay2 = Number(cuadrado2.dataset.y);
+  if (
+    (datax1 === datax2 && datay1 === datay2 + 1) ||
+    (datax1 === datax2 && datay1 === datay2 - 1) ||
+    (datay1 === datay2 && datax1 === datax2 + 1) ||
+    (datay1 === datay2 && datax1 === datax2 - 1)
+  ) {
+    console.log("son adyacentes");
+    return true;
+  } else {
+    console.log("NO son adyacentes");
+    return false;
+  }
+};
 
 // ------------------------ CLICKEAR EL EMOJI
 
@@ -160,22 +173,23 @@ const clickeable = () => {
 };
 
 // ------------------------------------INICIO MODALES
-// const modalBienvenida = document.querySelector("#contenedor-modal-bienvenida");
-// const AJugar = document.getElementById("boton-jugar");
+const modalBienvenida = document.querySelector("#contenedor-modal-bienvenida");
+const AJugar = document.getElementById("boton-jugar");
 // const botonCruz = document.querySelector(".delete");
- const modalDificultad = document.querySelector("#contenedor-modal-dificultad");
- console.log(modalDificultad)
- //const botonCerrarDificultad = document.querySelector("#cerrar-dificultad");
+const modalDificultad = document.querySelector("#contenedor-modal-dificultad");
+const modalDificultadInterior = document.querySelector(".modal-dificultad");
 
-// const ocultarBienvenida = () => {
- //  modalBienvenida.classList.add("ocultar");
- //};
+//const botonCerrarDificultad = document.querySelector("#cerrar-dificultad");
 
- const ocultarSeleccionDificultad = () => {
-   modalDificultad.classList.add("ocultar");
-   //modalDificultad.classList.remove("is-active");
- };
+const ocultarBienvenida = () => {
+  modalBienvenida.classList.add("ocultar");
+};
 
-// AJugar.onclick = () => {
-//   ocultarBienvenida();
-//};
+const ocultarSeleccionDificultad = () => {
+  modalDificultad.classList.add("ocultar");
+};
+
+AJugar.onclick = () => {
+  ocultarBienvenida();
+  modalDificultadInterior.classList.add("is-active");
+};
